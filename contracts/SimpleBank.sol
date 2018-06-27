8991e7b28ba0dc11cfef6f0de1b34344660f1d27
Nik Matveev <nnmatveev@gmail.com>
2018-06-27 21:48:16 +0300

    finished exercise

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -2,32 +2,38 @@ pragma solidity ^0.4.13;
 contract SimpleBank {
 
     /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
+    mapping(address => uint) balances;
 
     /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
     address owner;
 
     // Events - publicize actions to external listeners
     /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     // Constructor, can receive one or many variables here; only one allowed
     function SimpleBank() {
         /* Set the owner to the creator of this contract */
+        owner = msg.sender;
     }
 
     /// @notice Enroll a customer with the bank, giving them 1000 tokens for free
     /// @return The balance of the user after enrolling
     function enroll() public returns (uint){
-      /* Set the sender's balance to 1000, return the sender's balance */
+        /* Set the sender's balance to 1000, return the sender's balance */
+        balances[msg.sender] = 1000;
+        return balances[msg.sender];
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
     // Add the appropriate keyword so that this function can receive ether
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
         /* Add the amount to the user's balance, call the event associated with a deposit,
           then return the balance of the user */
+        balances[msg.sender] += msg.value;
+        emit LogDepositMade(msg.sender, msg.value);
+        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -39,7 +45,11 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. IF the send fails, add the amount back to the user's balance
            return the user's balance.*/
-
+        require(balances[msg.sender] >= withdrawAmount);
+        if (msg.sender.send(withdrawAmount)) {
+            balances[msg.sender] -= withdrawAmount;
+        }
+        return balances[msg.sender];
     }
 
     /// @notice Get balance
@@ -48,6 +58,7 @@ contract SimpleBank {
     // allows function to run locally/off blockchain
     function balance() public constant returns (uint) {
         /* Get the balance of the sender of this transaction */
+        return balances[msg.sender];
 
     }
 
